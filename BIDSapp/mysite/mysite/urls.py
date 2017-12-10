c896b2f74a760cd413da0e8152914251eaaf134c
NikkiBytes <nacosta2@uncc.edu>
2017-12-10 02:53:38 -0500

    added edits

--- BIDSapp/mysite/mysite/urls.py
+++ BIDSapp/mysite/mysite/urls.py
@@ -18,9 +18,8 @@ from django.urls import path, include
 from myapp.views import HomeView, DataView
 
 urlpatterns = [
-   # path('', HomeView.as_view()), 
-    path('', include('myapp.urls')),
-   # path('data/', include('myapp.urls')), #OsirixView.as_view()),
+    path('home/', include('myapp.urls')),
+   # path('data/', include('myapp.urls')),
    # path('home/', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
