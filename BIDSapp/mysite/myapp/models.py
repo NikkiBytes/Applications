515095622e1fc18851269d21408c98ea662c98a9
Live session user <NikkiBytes>
2017-12-26 03:20:57 +0000

    Add css style

--- BIDSapp/mysite/myapp/models.py
+++ BIDSapp/mysite/myapp/models.py
@@ -18,26 +18,4 @@ class OsirixModel(models.Model):
     sub_name = models.CharField(max_length=100)
     out_dir = models.CharField(max_length=200)
     #will have to add password/username
-'''class BIDSForm(ModelForm):
-    class Meta: 
-        model = BIDSModel 
-        fields = ['sub_name', 'in_dir', 'out_dir', 'multi_sess_pass'] 
 
-class OsirixForm(ModelForm):
-    class Meta:
-        model = OsirixModel 
-        fields = ['sub_name', 'out_dir']
-    
-
-class SubjectName(models.Model):
-    subject_name = models.CharField(max_length=200)
-
-    def get_absolute_url(self):
-        return reverse('bids-success')
-
-class OutputDirectory(models.Model):
-    output_directory = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.output_directory
-'''
